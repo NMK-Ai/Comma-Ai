319e095172e4d8f72528400abb2cb889fe7b4da4
Harald Schafer <harald.the.engineer@gmail.com>
2020-01-24 16:59:15 -0800

    no mpc for now

--- selfdrive/controls/lib/planner.py
+++ selfdrive/controls/lib/planner.py
@@ -93,10 +93,10 @@ class Planner():
   def choose_solution(self, v_cruise_setpoint, enabled):
     if enabled:
       solutions = {'model': self.v_model, 'cruise': self.v_cruise}
-      if self.mpc1.prev_lead_status:
-        solutions['mpc1'] = self.mpc1.v_mpc
-      if self.mpc2.prev_lead_status:
-        solutions['mpc2'] = self.mpc2.v_mpc
+      #if self.mpc1.prev_lead_status:
+      #  solutions['mpc1'] = self.mpc1.v_mpc
+      #if self.mpc2.prev_lead_status:
+      #  solutions['mpc2'] = self.mpc2.v_mpc
 
       slowest = min(solutions, key=solutions.get)
 
